8dc1bdaaaa7354e7aa82b302ffa082b76ed3a1fd
Conor Race (RIT Student) <ctr9664@rit.edu>
2024-03-08 14:41:00 -0500

    Finished template express api in-class demo.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -12,13 +12,44 @@ const notFound = (req, res) => {
 };
 
 const getData = (req, res) => {
-  const filtered = movies.filter(m => m.title === "Over the Hedge");
+  return res.json(movies);
+};
 
-  return res.json(filtered);
+const hostResults = (req, res) => {
+  if (!req.query.title && !req.query.year && !req.query.starring) {
+    return res.status(400).json({
+      message: "Must provide at least one search parameter.",
+      id: "resultsMissingTerm",
+    });
+  }
+  
+  let results = movies;
+
+  if (req.query.title) {
+    results = results.filter(m => 
+      m.title.toLowerCase()
+      .includes(req.query.title.toLowerCase())
+    );
+  }
+
+  if (req.query.year) {
+    const year = parseInt(req.query.year, 10);
+    results = results.filter(m => m.year === year);
+  }
+
+  if (req.query.starring) {
+    results = results.filter(m => m.cast.includes(req.query.starring));
+  }
+
+  return res.render('results', {
+    movies: results,
+    search: req.query,
+  });
 };
 
 module.exports = {
   index: hostIndex,
   notFound,
   getData,
+  results: hostResults,
 };
--- server/router.js
+++ server/router.js
@@ -3,6 +3,7 @@ const controllers = require('./controllers');
 const router = (app) => {
   app.get('/', controllers.index);
   app.get('/getData', controllers.getData);
+  app.get('/results', controllers.results);
 
   app.get('/*', controllers.notFound);
 };
